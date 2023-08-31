268211fed32d566934e75bb26c2e75223fb1bc87
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-09-01 06:06:26 +0800

    Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { Article, Brand, CTA, Feature, Navbar } from "./components/index";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers/index";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
