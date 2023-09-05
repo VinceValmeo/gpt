5f7ab9d1745375c7dc1f6b646a07e86266cf04f1
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-09-05 16:55:25 +0800

    Complete possibilities

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import possibility from "../../assets/possibility.png";
+import "./possibility.css";
 
 function Possibility() {
-  return <div>Possibility</div>;
+  return (
+    <div className="possibility__container">
+      <div className="possibility__image">
+        <img src={possibility} />
+      </div>
+      <div className="possiblity__maintext">
+        <h1 className="possibility__header gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p className="possibility__maintext">
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+          <p className="gradient__text early__access">
+            Request Early Access to Get Started
+          </p>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Possibility;
