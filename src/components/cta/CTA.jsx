8d5ea3e49b7e4d738f8c36fc827e24002d4eb3f8
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-09-05 17:40:30 +0800

    Complete CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./cta.css";
 
 function CTA() {
-  return <div>CTA</div>;
+  return (
+    <div className="CTA__container">
+      <div className="CTA__parent">
+        <div className="CTA__text">
+          <p>Request Early Access to Get Started</p>
+        </div>
+        <div className="CTA__attention">
+          <h1>Register today & start exploring the endless possiblities.</h1>
+          <button type="button" className="button__start">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CTA;
